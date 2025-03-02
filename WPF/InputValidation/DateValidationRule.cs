e845ae955682f794b395720769bd29ced2e20658
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-03-02 16:54:34 +0100

    -

---
+++ WPF/InputValidation/DateValidationRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace InputValidation
+{
+    public class DateValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null || !(value is DateTime))
+            {
+                return new ValidationResult(false, "Invalid date.");
+            }
+
+            DateTime selectedDate = (DateTime)value;
+            if (selectedDate < DateTime.Today) // Example: Disallow past dates
+            {
+                return new ValidationResult(false, "Date cannot be in the past.");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ WPF/InputValidation/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InputValidation
+{
+    public class MainWindowViewModel
+    {
+        private DateTime _myDate;
+
+        public DateTime MyDate
+        {
+            get => _myDate;
+            set => _myDate = value;
+        }
+    }
+}
