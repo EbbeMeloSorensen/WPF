89292b603983423bb996df3abcf0a8e139f8ccff
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-03-07 17:16:19 +0100

    -

--- WPF/InputValidation/MainWindowViewModel.cs
+++ WPF/InputValidation/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using InputValidation.View2;
 using InputValidation.View3;
 using InputValidation.View4;
+using InputValidation.View5;
 
 namespace InputValidation
 {
@@ -11,6 +12,7 @@ namespace InputValidation
         public ViewModel2 ViewModel2 { get; }
         public ViewModel3 ViewModel3 { get; }
         public ViewModel4 ViewModel4 { get; }
+        public ViewModel5 ViewModel5 { get; }
 
         public MainWindowViewModel()
         {
@@ -18,6 +20,7 @@ namespace InputValidation
             ViewModel2 = new ViewModel2();
             ViewModel3 = new ViewModel3();
             ViewModel4 = new ViewModel4();
+            ViewModel5 = new ViewModel5();
         }
     }
 }
