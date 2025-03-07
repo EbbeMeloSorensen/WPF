da31f839db74f91663ecc7482cad00aedc41a2b3
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-03-07 20:13:34 +0100

    -

---
+++ WPF/InputValidation2/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace InputValidation2
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private DateTime? _myDate;
+
+        public DateTime? MyDate
+        {
+            get => _myDate;
+            set
+            {
+                _myDate = value;
+                OnPropertyChanged(nameof(MyDate));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == nameof(MyDate))
+                {
+                    if (MyDate == null)
+                    {
+                        return "Date is required.";
+                    }
+                    if (MyDate < DateTime.Today)
+                    {
+                        return "Date cannot be in the past.";
+                    }
+                }
+                return null;
+            }
+        }
+
+        public string Error => null;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
