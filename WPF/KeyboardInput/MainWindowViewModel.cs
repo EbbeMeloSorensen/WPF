95d564591883b88ee5982b27c036b1f36d9ee41e
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-11-23 15:13:15 +0100

    -

---
+++ WPF/KeyboardInput/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace KeyboardInput
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private RelayCommand _dummyCommandMainWindowViewModel;
+
+        public SubViewModel SubViewModel { get; }
+
+        public RelayCommand DummyCommandMainWindowViewModel =>
+            _dummyCommandMainWindowViewModel ?? (_dummyCommandMainWindowViewModel =
+                new RelayCommand(DummyMethodImMainViewModel));
+
+        private void DummyMethodImMainViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MainWindowViewModel()
+        {
+            SubViewModel = new SubViewModel();
+        }
+    }
+}
---
+++ WPF/KeyboardInput/SubViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace KeyboardInput
+{
+    public class SubViewModel : ViewModelBase
+    {
+        private RelayCommand _dummyCommandInSubViewModel;
+
+        public RelayCommand DummyCommandInSubViewModel =>
+            _dummyCommandInSubViewModel ?? (_dummyCommandInSubViewModel =
+                new RelayCommand(DummyMethodInSubViewModel));
+
+        private void DummyMethodInSubViewModel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
