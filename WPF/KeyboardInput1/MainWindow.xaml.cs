182a94a2563d4444ec4a88d3d49cc9c0614e5415
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-11-24 20:35:01 +0100

    -

---
+++ WPF/KeyboardInput1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardInput1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SubviewViewModel Subview { get; } = new SubviewViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            // IMPORTANT: move focus to content on startup
+            Loaded += (_, __) => Keyboard.Focus((UIElement)Content);
+        }
+    }
+}
---
+++ WPF/KeyboardInput1/SubviewView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardInput1
+{
+    /// <summary>
+    /// Interaction logic for SubviewView.xaml
+    /// </summary>
+    public partial class SubviewView : UserControl
+    {
+        public SubviewView()
+        {
+            InitializeComponent();
+            this.PreviewKeyDown += OnPreviewKeyDown;
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            Debug.WriteLine($"SubviewView received key: {e.Key}");
+        }
+    }
+}
